86eaa25d895f021a227f55c12a1d8d6acc0a35b4
OmkarSangaonkar <omsangaonkar@gmail.com>
2022-01-25 23:12:56 +0530

    added index.html and Circle.js

---
+++ Circle.js
@@ -0,0 +1,8 @@
+// Area of circle
+
+const CircleAreaPerim = (radius) => {
+  console.log(`Area of circle is : ${Math.PI * (radius * radius)}`);
+  console.log(`Perimeter of circle is : ${2 * Math.PI * radius}`);
+};
+
+CircleAreaPerim(10);
